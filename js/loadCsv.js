8b0f0920c40f548c7b94e14cc75c20d43fb09cd4
ITProfessor40148 <SweetHeart40148@outlook.com>
2020-02-13 16:41:30 +0800

    "remove new line"

--- js/loadCsv.js
+++ js/loadCsv.js
@@ -6,11 +6,21 @@ async function loadInitData() {
     let file_propertise = "\\datas\\properties.csv";
     await $.get(file_channel, function(data) {
         initialData["csvDataOfChannel"] = Papa.parse(data).data;
+        removeEmptyLineData(initialData.csvDataOfChannel);
+        console.log(initialData.csvDataOfChannel);
     });
     await $.get(file_product, function(data) {
         initialData["csvDataOfProduct"] = Papa.parse(data).data;
+        removeEmptyLineData(initialData.csvDataOfProduct);
     });
     await $.get(file_propertise, function(data) {
         initialData["csvDataOfPropertise"] = Papa.parse(data).data;
+        removeEmptyLineData(initialData.csvDataOfPropertise);
     });
+}
+
+function removeEmptyLineData(data) {
+    while (data[data.length - 1].length === 1) {
+        data.splice(-1);
+    }
 }
